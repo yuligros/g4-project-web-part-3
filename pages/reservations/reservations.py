28f66e9f52a65a528d58d9a35c4c1cdb3d8bc86c
Ido Polak <Idop93@gmail.com>
2022-02-01 20:50:24 +0200

    Ido - updated homepage and reservations

--- pages/reservations/reservations.py
+++ pages/reservations/reservations.py
@@ -11,7 +11,7 @@ def index():
     if(session['logged_in']):
         user_id=session['id']
         print(user_id)
-        user_res = dbManager.fetch("select b.user_ID, r.restaurants_name, b.booking_Date, b.booking_Time, b.number_of_diners, r.website, r.phone_number, l.city from bookings as b join tabels as t on b.table_ID = t.table_ID join restaurants as r on t.restaurants_ID = r.restaurants_ID join locations as l on r.location_ID = l.location_ID where b.user_ID =('%s');"%user_id)
+        user_res = dbManager.fetch("select b.user_ID, r.restaurants_name, b.booking_Date, b.booking_Time, b.number_of_diners, r.website, r.phone_number, l.city, r.img from bookings as b join tabels as t on b.table_ID = t.table_ID join restaurants as r on t.restaurants_ID = r.restaurants_ID join locations as l on r.location_ID = l.location_ID where b.user_ID =('%s');"%user_id)
         print(user_res)
         return render_template('reservations.html', reservations = user_res)
     else:
