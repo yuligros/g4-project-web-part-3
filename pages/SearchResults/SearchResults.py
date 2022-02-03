e5c4d31d6c1131e737257a6436bdb73d7f052fed
yuligros <yuligros11@gmail.com>
2022-02-03 14:34:19 +0200

    yulis

--- pages/SearchResults/SearchResults.py
+++ pages/SearchResults/SearchResults.py
@@ -10,19 +10,18 @@ def index():
 
     if request.method == 'POST':
 
-        seating_opt = request.values.get('seating_opt')
-        regions = request.values.get('regions')
+        tables_after_filters = []
+        seating_opt = str(request.values.get('seating_opt'))
         cuisine = request.values.get('cuisine')
         rating = request.values.get('rating')
-        tables = request.values.get('tables')
+        tables = session['free_tables']
 
-        print(seating_opt)
-        print(regions)
-        print(cuisine)
-        print(rating)
-        print(tables)
+        for table in tables:
+            if table[9] == seating_opt and table[2] == cuisine and table[3] >= rating:
+                tables_after_filters.append(table)
 
+        print(tables_after_filters)
+        return  render_template('SearchResults.html', tables_after_filters = tables_after_filters)
+    else:
         return render_template('SearchResults.html')
 
-
-    return render_template('SearchResults.html')
